e9e08a19d68b038207024de2fabf63093da31029
ileler <ileler@qq.com>
2019-09-03 17:51:46 +0800

    fix for-webflux error

--- src/main/java/org/ileler/swjp/JsonParamConfigForWebFlux.java
+++ src/main/java/org/ileler/swjp/JsonParamConfigForWebFlux.java
@@ -4,12 +4,17 @@ import com.jayway.jsonpath.JsonPath;
 import org.springframework.core.MethodParameter;
 import org.springframework.core.ReactiveAdapterRegistry;
 import org.springframework.http.codec.ServerCodecConfigurer;
+import org.springframework.ui.Model;
 import org.springframework.web.reactive.BindingContext;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
 import org.springframework.web.reactive.result.method.annotation.AbstractMessageReaderArgumentResolver;
 import org.springframework.web.reactive.result.method.annotation.ArgumentResolverConfigurer;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
+import reactor.core.publisher.MonoSink;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Author: kerwin612@qq.com
@@ -23,6 +28,8 @@ public class JsonParamConfigForWebFlux implements WebFluxConfigurer {
 
     class HandlerMethodArgumentResolverForFlux extends AbstractMessageReaderArgumentResolver {
 
+        private static final String JSONBODYATTRIBUTE = "JSON_REQUEST_BODY";
+
         public HandlerMethodArgumentResolverForFlux() {
             super(ServerCodecConfigurer.create().getReaders(), ReactiveAdapterRegistry.getSharedInstance());
         }
@@ -34,23 +41,20 @@ public class JsonParamConfigForWebFlux implements WebFluxConfigurer {
 
         public Mono<Object> resolveArgument(MethodParameter methodParameter, BindingContext bindingContext, ServerWebExchange serverWebExchange) {
             JsonParamBean jsonParamBean = JsonParamResolver.resolveArgument(methodParameter);
-//            ServerWebExchange exchange = serverWebExchange.mutate().request(new ServerHttpRequestDecorator(serverWebExchange.getRequest()) {
-//
-//                private DataBuffer cachedBuffer;
-//
-//                @Override
-//                public Flux<DataBuffer> getBody() {
-//                    return cachedBuffer != null ? Flux.just(cachedBuffer) : super.getBody()
-//                            .publishOn(single())
-//                            .doOnNext(this::cache);
-//                }
-//
-//                private void cache(DataBuffer buffer) {
-//                    cachedBuffer = buffer;
-//                }
-//
-//            }).build();
-            return readBody(methodParameter, false, bindingContext, serverWebExchange).map(json -> JsonPath.read((String) json, jsonParamBean.getParamName())).defaultIfEmpty(jsonParamBean.getParamDefaultValue());
+            Model model = bindingContext.getModel();
+            return Mono.defer(() -> {
+                if (model.asMap().containsKey(JSONBODYATTRIBUTE)) {
+                    return Mono.create(
+                            (callback -> ((Map<String, MonoSink<Object>>) model.asMap().get(JSONBODYATTRIBUTE)).put(jsonParamBean.getParamName(), callback))
+                    ).defaultIfEmpty(jsonParamBean.getParamDefaultValue());
+                }
+                model.addAttribute(JSONBODYATTRIBUTE, new HashMap<String, MonoSink<Object>>());
+                return readBody(methodParameter, false, bindingContext, serverWebExchange).map(json -> {
+                    ((Map<String, MonoSink<Object>>) model.asMap().get(JSONBODYATTRIBUTE)).forEach(
+                            (String paramName, MonoSink<Object> callback) -> callback.success(JsonPath.read((String) json, paramName)));
+                    return JsonPath.read((String) json, jsonParamBean.getParamName());
+                }).defaultIfEmpty(jsonParamBean.getParamDefaultValue());
+            });
         }
 
     }
